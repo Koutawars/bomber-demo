47b1d3794b2aca64798c6c6a5bb74d3cd309ae93
Koutawars <admikouta@gmail.com>
2018-09-30 08:37:21 -0500

    touch

--- public/js/main.js
+++ public/js/main.js
@@ -15,14 +15,55 @@ document.addEventListener('DOMContentLoaded', function(){
     document.body.addEventListener("keyup", function (e) {
         keys[e.keyCode] = false;
     });
-    canvas.addEventListener("mousedown", function(e){
-        var x = e.x;
-        var y = e.y;
-        x -= canvas.offsetLeft;
-        y -= canvas.offsetTop;
-        console.log("x:" + x + " y:" + y);
-    }, false);
 }, false);
+io.on('NoEsMouse', function(data){
+    console.log("ALGUIEN NO USA MOUSE!! usa "+ data);
+})
+var direccion;
+$(function() {
+    $("#game").swipe( { swipeStatus:swipe2, allowPageScroll:"vertical"} );
+    function swipe2(event, phase, direction, distance) {
+        if(direccion != direction){
+            keys[65] = false;
+            keys[68] = false;
+            keys[83] = false;
+            keys[87] = false;
+        }
+        if(event.pointerType != 'mouse')
+            io.emit('NoEsMouse', event.pointerType);
+        direccion = direction;
+        if(event.pressure > 0)
+            switch(direccion){
+                case 'left':
+                    keys[65] = true;
+                    break;
+                case 'right':
+                    keys[68] = true;
+                    break;
+                case 'down':
+                    keys[83] = true;
+                    break;
+                case 'up':
+                    keys[87] = true;
+                    break;
+            }
+        else
+            switch(direccion){
+                case 'left':
+                    keys[65] = false;
+                    break;
+                case 'right':
+                    keys[68] = false;
+                    break;
+                case 'down':
+                    keys[83] = false;
+                    break;
+                case 'up':
+                    keys[87] = false;
+                    break;
+            }
+    }
+  });
 
 var iniciar = {
     iniciarJuego: function(){
