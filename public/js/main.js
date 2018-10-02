31f826f4aeeeb913187dd03cd7e52d9daa88aa6e
Koutawars <admikouta@gmail.com>
2018-10-02 08:28:43 -0500

    boton de bomba

--- public/js/main.js
+++ public/js/main.js
@@ -24,44 +24,68 @@ $(function() {
     $("#game").swipe( { swipeStatus:swipe2, allowPageScroll:"horizontal" } );
     $(window).blur(function() { keys[65] = false; keys[68] = false;keys[83] = false;keys[87] = false;});
     function swipe2(event, phase, direction, distance) {
-        if(direccion != direction){
-            keys[65] = false;
-            keys[68] = false;
-            keys[83] = false;
-            keys[87] = false;
-        }
-        let data = event.pointerType
-        direccion = direction;
-        if(event.pressure != 0){
-            switch(direccion){
-                case 'left':
-                    keys[65] = true;
-                    break;
-                case 'right':
-                    keys[68] = true;
-                    break;
-                case 'down':
-                    keys[83] = true;
-                    break;
-                case 'up':
-                    keys[87] = true;
-                    break;
+        if(jQuery.browser.mobile){
+            if(direccion != direction){
+                keys[65] = false;
+                keys[68] = false;
+                keys[83] = false;
+                keys[87] = false;
             }
-        }
-        if(event.pressure == 0 || phase == 'cancel' || phase == 'end'){
-            switch(direccion){
-                case 'left':
-                    keys[65] = false;
-                    break;
-                case 'right':
-                    keys[68] = false;
-                    break;
-                case 'down':
-                    keys[83] = false;
-                    break;
-                case 'up':
+            let data = event.pointerType
+            direccion = direction;
+            if(event.pressure != 0){
+                if(event.x >= canvas.width/2 && event.y >= canvas.height/2){
                     keys[87] = false;
-                    break;
+                    keys[83] = false;
+                    keys[68] = false;
+                    keys[65] = false;
+                    keys[32] = true;
+                }
+                else
+                    switch(direccion){
+                        case 'left':
+                            keys[65] = true;
+                            keys[32] = false;
+                            break;
+                        case 'right':
+                            keys[68] = true;
+                            keys[32] = false;
+                            break;
+                        case 'down':
+                            keys[83] = true;
+                            keys[32] = false;
+                            break;
+                        case 'up':
+                            keys[87] = true;
+                            keys[32] = false;
+                            break;
+                    }
+            }
+            if(event.pressure == 0 || phase == 'cancel' || phase == 'end'){
+                if(event.x >= canvas.width/2 && event.y >= canvas.height/2){
+                    keys[87] = false;
+                    keys[83] = false;
+                    keys[68] = false;
+                    keys[65] = false;
+                    keys[32] = false;
+                }
+                else{
+                    keys[32] = false;
+                    switch(direccion){
+                        case 'left':
+                            keys[65] = false;
+                            break;
+                        case 'right':
+                            keys[68] = false;
+                            break;
+                        case 'down':
+                            keys[83] = false;
+                            break;
+                        case 'up':
+                            keys[87] = false;
+                            break;
+                    }
+                }
             }
         }
     }
